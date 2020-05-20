fd63efc5b8fce81e4452ff8c0578a3d5f527058d
DE0414\User <aslandukov-matvej@yandex.ua>
2020-05-20 19:34:47 +0300

    Improved perfomance

--- main.py
+++ main.py
@@ -228,4 +228,4 @@ def recieve_move(data, methods=['GET', 'POST']):
     
     
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=False)
